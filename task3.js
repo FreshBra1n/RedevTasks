bfdeef88c6736a0987d268d04869791cc09b62ec
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-03-04 18:47:07 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,6 +1,9 @@
 //task 3
 
+function list (str){
+    let newStr=str.split(' ');
+    return (`Min: ${Math.min(...newStr)}, Max: ${Math.max(...newStr)}`)
+}
 let str = "4 5 29 54 4 0 -123 666 -64 1 -3 6 -6"
 
-const arr = str.split(' ');
-console.log(`Min: ${Math.min(...arr)}, Max: ${Math.max(...arr)}`)
+console.log(list(str))
