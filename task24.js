b225707f7cd8d9c451fd86b76af134f1e40a926d
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-13 15:25:35 +0300

    Update task24.js

--- task24.js
+++ task24.js
@@ -1,9 +1,15 @@
+// let str = 'tasks JavaScript'
+// function isUpperCase(str, character) {
+//     if (str[character].replace(/\A-Z/)==str[character].toUpperCase()) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isUpperCase(str,6))
+
 let str = 'tasks JavaScript'
-function isUpperCase(str, character) {
-    if (str[character].replace(/\A-Z/)==str[character].toUpperCase()) {
-        return true
-    } else {
-        return false
-    }
-}
+ isUpperCase=(str, character)=>(str[character]==str[character].toUpperCase())
+  
 console.log(isUpperCase(str,6))
+
