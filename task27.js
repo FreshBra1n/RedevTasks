5d4caaca7779b1dbc67b394ae1dd7ff24dc4b7b0
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-19 18:00:39 +0300

    Update task27.js

--- task27.js
+++ task27.js
@@ -1,10 +1,17 @@
+// let str = 'abc def def lom abc abc def';
+
+// function replaceAll(find, replace, str) {
+//  while(str.indexOf(find) > -1){
+//      str=str.replace(find,replace)
+//  }
+//  return str
+// };
+
+// console.log(replaceAll('abc', 'x', str))
+
 let str = 'abc def def lom abc abc def';
 
-function replaceAll(find, replace, str) {
- while(str.indexOf(find) > -1){
-     str=str.replace(find,replace)
- }
- return str
-};
-
+function replaceAll(search, replace, str) {
+    return str.split(search).join(replace);
+}
 console.log(replaceAll('abc', 'x', str))
