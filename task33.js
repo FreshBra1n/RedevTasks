64ea9b1256a8da87bfae24117548cc3f6e8e0e94
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-21 17:07:08 +0300

    Update task33.js

--- task33.js
+++ task33.js
@@ -1,15 +1,19 @@
-function toLower(str) {
-    return str.toLowerCase()
-}
+// function toLower(str) {
+//     return str.toLowerCase()
+// }
 
-function comparison(str1, str2) {
-    str1 = toLower(str1)
-    str2 = toLower(str2)
-    if (str1 == str2) {
-        return true
-    } else {
-        return false
-    }
-};
+// function comparison(str1, str2) {
+//     str1 = toLower(str1)
+//     str2 = toLower(str2)
+//     if (str1 == str2) {
+//         return true
+//     } else {
+//         return false
+//     }
+// };
+
+// console.log(comparison('string', 'StRiNg'))
+
+const comparison=(str1, str2)=> (str1.toLowerCase()==str2.toLowerCase())?true:false
 
 console.log(comparison('string', 'StRiNg'))
