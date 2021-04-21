45932e0b95319b6fdbde23e0e2362db5db77605a
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-21 16:56:27 +0300

    Create task33.js

---
+++ task33.js
@@ -0,0 +1,15 @@
+function toLower(str) {
+    return str.toLowerCase()
+}
+
+function comparison(str1, str2) {
+    str1 = toLower(str1)
+    str2 = toLower(str2)
+    if (str1 == str2) {
+        return true
+    } else {
+        return false
+    }
+};
+
+console.log(comparison('string', 'StRiNg'))
