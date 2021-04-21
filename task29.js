272879fa8663ddb67fba4efe49851e505cbf7cfa
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-21 17:21:51 +0300

    Update task29.js

--- task29.js
+++ task29.js
@@ -1,11 +1,23 @@
 function unique_letters(str) {
-str=str.split('')
-let arr=[]
-for (let i = 0; i < str.length; i++) {
-  if(arr.indexOf(str[i])==-1) arr.push(str[i])    
-}
-   return arr=arr.join('')
+    str=str.split('')
+    const uSet =new Set(str)
+    const toArr=[...uSet]
+   return toArr.join('')
 
 } 
 
 console.log (unique_letters('anaconda'))
+
+
+
+// function unique_letters(str) {
+// str=str.split('')
+// let arr=[]
+// for (let i = 0; i < str.length; i++) {
+//   if(arr.indexOf(str[i])==-1) arr.push(str[i])    
+// }
+//    return arr=arr.join('')
+
+// } 
+
+// console.log (unique_letters('anaconda'))
