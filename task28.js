bfd67af1b59dc6d6b18ec2393434f59e703273f9
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-04-21 17:23:30 +0300

    Update task28.js

--- task28.js
+++ task28.js
@@ -1,12 +1,23 @@
 let str = "вишня, груша, слива, груша"
 
 function removeDuplicate(str) {
- str=str.split(',')
- let arr=[]
- for (let i = 0; i < str.length; i++) {
-   if(arr.indexOf(str[i])==-1) arr.push(str[i])    
- }
-    return arr=arr.join(',')
+    str=str.split(',')
+    const uSet =new Set(str)
+    const toArr=[...uSet]
+   return toArr.join(',')
+
 }
 
 console.log(removeDuplicate(str))
+
+
+// function removeDuplicate(str) {
+//  str=str.split(',')
+//  let arr=[]
+//  for (let i = 0; i < str.length; i++) {
+//    if(arr.indexOf(str[i])==-1) arr.push(str[i])    
+//  }
+//     return arr=arr.join(',')
+// }
+
+// console.log(removeDuplicate(str))
