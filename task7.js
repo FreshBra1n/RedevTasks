fd88958333243448ab8eee5b834d8e7930122f62
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-05-18 23:45:08 +0300

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,6 +1,6 @@
-String.prototype.toJadenCase=function (){
-    this.split(' ')
-    return   str.map(n=>`${n[0].toUpperCase()}${n.slice(1)}`).join(' ')
+String.prototype.toJadenCase = function () {
+    let str = this.split(' ')
+    return str.map(n => `${n[0].toUpperCase()}${n.slice(1)}`).join(' ')
 }
 
 console.log('народ прив'.toJadenCase())
