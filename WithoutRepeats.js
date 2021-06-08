aef1e559127f136353a715217445a02035ad9c51
Aleksandr Masyk <69385269+FreshBra1n@users.noreply.github.com>
2021-06-08 19:39:51 +0300

    Update WithoutRepeats.js

--- WithoutRepeats.js
+++ WithoutRepeats.js
@@ -18,3 +18,29 @@ function withOutRepeat(array) {
 }
 
 console.log(withOutRepeat(arr))
+
+
+let arr = [1, 2, 3, 4, 5, 2, 21, 4, 4, 5, 6]
+
+function withOutRepeat2(array) {
+    let clerArray = [];
+    let obj = {};
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (!(element in obj)) {
+            obj[element] = 1
+        } else {
+            obj[element] += 1
+        }
+    }
+    const keys = Object.keys(obj)
+    keys.forEach((el) => {
+        if (obj[el] === 1){
+            clerArray.push(el)
+        }
+    })
+return clerArray
+}
+
+console.log(withOutRepeat2(arr))
